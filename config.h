db951927d96f2a68040c40331ca4d874ac6f44a0
Your Name <you@example.com>
2022-10-20 22:05:32 +0300

    ye

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 3;
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 0;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -13,6 +13,10 @@ static const char *fonts[] = {
   "FontAwesome5FreeSolid:pixelsize=15:antialias=true:autohint=true",
   "FontAwesome5Brands:pixelsize=15:antialias=true:autohint=true"
 };
+
+#include "/home/void/.cache/wal/colors-wal-dwm.h"
+
+/*
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -20,13 +24,14 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#ff5500";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
+	 =               fg         bg         border   =
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+*/
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
+static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -34,8 +39,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "Firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
+  { "Firefox",      NULL,       NULL,       1 << 1,       0,           -1 },
+  { "pavucontrol",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "discord",      NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
@@ -63,7 +69,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", "monospace:size=13", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", "monospace:size=13", };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
